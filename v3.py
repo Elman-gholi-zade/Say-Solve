6914c276e694b2c2c25feccfd86e033d8c07746f
Elman_gholi_zade <zr1390_codeQgmail.com>
2025-12-31 12:20:34 +0330

    Design min meno and history meno + Show all history section from the history  (v3.5)

--- v3.py
+++ v3.py
@@ -75,12 +75,37 @@ class Calculator :
 class HistoryManager :
   
     def __init__(self):
-        pass
+       self.__history = self.__load_history()    # Load History File
         
 
 
+    # History Section Meno
+    def history_meno(self) :
+        '''منوی بخش تاریخچه'''
+
+        print("\n\n==== History Meno =====================")
+        print("  1. Show All \n  2. Show Last \n  3. Delete All \n")
+        history_meno = input(" >>>  ")
+
+
+        if history_meno == "1" :
+            self.__show_all_history()
+
+
+
+    # Automatic saving new record in history
+    def auto_save_history(self, operation, num1, num2, result) :
+        '''این متد با کنار هم گذاشتن
+        چندین متد, ورودی های یک عملیات را 
+        به صورت خودکار در فایل تاریخچه ذخیره
+        می کند'''
+
+        self.__add_to_history(self.__build_record(operation, num1, num2, result))
+
+
+
     # Load History File
-    def load_history(self) :
+    def __load_history(self) :
         '''این تابع فایل تاریخچه
         را لود می کند'''
 
@@ -103,17 +128,17 @@ class HistoryManager :
     
 
     # Save History File
-    def save_history(self, history) :
+    def __save_history(self) :
         '''این تابع تاریخچه جدید 
         را به فایل تاریخچه اضافه می کند'''
 
         with open("history.json", "w", encoding="utf-8") as file :
-            json.dump(history, file, ensure_ascii=False, indent=4)
+            json.dump(self.__history, file, ensure_ascii=False, indent=4)
 
 
 
     # Structuring
-    def build_record(self, operation, num1, num2, result) :
+    def __build_record(self, operation, num1, num2, result) :
         '''این تابع بخش های مختلف یک عملیات را
         ساختاردهی می کند'''
 
@@ -129,20 +154,52 @@ class HistoryManager :
 
 
     # Add and save new history
-    def add_to_history(self, record) :
+    def __add_to_history(self, record) :
         '''.این تابع ابتدا فایل تاریخچه را لود کرده
          سپس عملیات انجام و ساختاردهی شده را
          که از ورودی می گیرد, در فایل تاریخچه
          دخیره می کند'''
         
-
-        # load history file
-        history = self.load_history()
-
         # Add and save
-        history.append(record)
+        self.__history.append(record)
 
-        self.save_history(history)
+        self.__save_history()
+
+
+
+
+
+
+    # Show all records in history
+    def __show_all_history(self) :
+        '''همه عملیات داخل تاریخچه را شماره بندی کرده
+         می دهد
+        |به متد |فرمت بندی برای نمایش 
+        می دهد تا برای نمایش آماده کند و در
+        نتیجه آن را نمایش می دهد'''
+
+        print("\n\n======================================================================")
+        print("============================ History =================================\n")
+        for index, record in enumerate (self.__history, start=1) :
+            print("\n", self.display_formatting(index, record))
+
+        print("======================================================================\n\n")
+
+    
+
+    # Formatting history ot show
+    def display_formatting(self, index, record) :
+        '''باز کردن تاریخچه و فرمت بندی و همچنین
+        شماره بندی'''
+
+        return f"{index}|> {record['num1']} {record['operation']} {record['num2']} = {record['result']}"
+
+
+
+
+
+      
+
 
 
 
@@ -180,19 +237,44 @@ def connections(operation, num1, num2) :
         
     operations[operation]()
 
-    record = history.build_record(operation, num1, num2, calc.result)
 
 
     # Show result
     print("----------------------------------")
-    print(calc.show_result(record))
+    # print(calc.show_result(record))
     print("----------------------------------")
 
 
     # save in history
-    history.add_to_history(record)
+    history.auto_save_history(operation, num1, num2, calc.result)
 
 
+
+
+
+
+
+# =============================
+# ======== Min Meno ===========
+# =============================
+def min_meno() :
+    '''منو اصلی'''
+
+    print("\n\n\n======================= Say & Solve ====================\n")
+
+    while True :
+        print("| 1. Calculator \n| 2. History \n| 3. Exit \n")
+        user_choose = input("| >>>  ")
+
+        if user_choose == "1" :
+            op, num1, num2 = get_inputs()
+            connections(op, num1, num2)
+
+        elif user_choose == "2" :
+            HistoryManager().history_meno()
+        
+        elif user_choose == "3" :
+            break
 
 
 
@@ -240,6 +322,4 @@ def get_inputs() :
 # ========================
 # ======== Run ===========
 # ========================
-while True :
-    op, num1, num2 = get_inputs()
-    connections(op, num1, num2)
+min_meno()
